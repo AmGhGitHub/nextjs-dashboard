d9a208e3b1fa8d468b87d75659ec4e448a21de4a
Seyyed Ghaderi <sghaderi6@gatech.edu>
2023-11-10 06:55:28 -0700

    init

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>invoice spage</div>;
+};
+
+export default Page;
---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+// import { Lusitana } from 'next/font/google';
+// import { lusitana } from '@/app/ui/fonts';
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
